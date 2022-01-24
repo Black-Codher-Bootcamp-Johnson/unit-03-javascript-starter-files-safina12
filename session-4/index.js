4db40556e6536fe2edfb95410b77e7da7fac1211
Safina12 <safinadahou@hotmail.co.uk>
2022-01-24 20:35:52 +0000

    functions with concatonate and maths

--- session-4/index.js
+++ session-4/index.js
@@ -1,50 +1,89 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA={
+    name: 'Sasha',
+    age: 30,
+    location: 'Miami',
+    likes: 'martinis'
+}
 
+let personB={
+    name: 'Tammy',
+    age: 29,
+    location: 'Paris',
+    likes: 'pina colada'
+}
 
+let personC={
+    name: 'Cam',
+    age: 21,
+    location: 'London',
+    likes: 'mojito'
+}
 
+let personD={
+    name: 'Rachel',
+    age: 29,
+    location: 'Spain',
+    likes: 'prosecco'
+}
 
+function biography(person){
+    console.log('Hi! My name is ' + person.name + ' and I am ' + person.age + ' years old, I live in ' + person.location + ' and I like ' + person.likes + '.')
 
+}
 
-
-
-
-
-
-
-
-
+biography(personA)
+biography(personB)
+biography(personC)
+biography(personD)
 
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y= " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y= " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y= " + multiplication);
 
-// const division = x * y;
+const division = x / y;
 
-// console.log("Division: x + y " + division);
-
-
+console.log("Division: x / y= " + division);
 
 
+const exponentiation = x ** y;
+
+console.log('exponentiation: x ** y= '+ exponentiation);
+
+const addition3 = x + x + y 
+
+console.log('addition: x + x + y= ' + addition3);
+
+const multiplication3 = x * y / x 
+
+console.log('multiplication3: x * x / y= ' + multiplication3);
 
 
 
 
+
+
+const age = 21;
+const minage = 17;
+
+const isoldenough = age > minage;
+console.log('Am I old enough to drive? ' + isoldenough);
 
 
 
